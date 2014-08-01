3fa295ad4cb8a67945b324fba098671fff3ce90a
Shubhang Rathore <shubhang92@gmail.com>
2014-08-01 13:29:05 +0530

    Remove "Exit" button from Keep Trash action bar
    Using "Exit" doesn't comply with Google's guidelines
    for android app GUI.

--- app/src/main/java/com/shubhangrathore/xposed/keeptrash/MainActivity.java
+++ app/src/main/java/com/shubhangrathore/xposed/keeptrash/MainActivity.java
@@ -99,25 +99,6 @@ public class MainActivity extends PreferenceActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.keep_trash_options_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_exit) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     /**
      * Retrieve Keep Trash app version
